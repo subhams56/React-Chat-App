fc2b78e4dcf47d85ce80d4ac8b9ac6ca2d4c691a
Subham Saha <89207473+subhams56@users.noreply.github.com>
2022-10-31 02:26:24 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmejZh7Jkg4QLYY1vb5yJ9iK4xkqIGk4c",
+  authDomain: "subhamsaha-chatapp.firebaseapp.com",
+  projectId: "subhamsaha-chatapp",
+  storageBucket: "subhamsaha-chatapp.appspot.com",
+  messagingSenderId: "861346237396",
+  appId: "1:861346237396:web:7d804f00072e0fad6d6441"
+};
+
+
+export const app = initializeApp(firebaseConfig);
